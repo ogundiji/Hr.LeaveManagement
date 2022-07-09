cd80f3a1f1118d4a6170ff920841ee026ce42b9c
Yusuf Ogundiji <ogundijiyusuf@gmail.com>
2022-07-09 14:27:45 +0100

    add Account controller

--- Hr.LeaveManagement.Api/Controllers/AccountController.cs
+++ Hr.LeaveManagement.Api/Controllers/AccountController.cs
@@ -1,6 +1,8 @@
 ﻿using HR.LeaveManagement.Application.Contracts.Identity;
+using HR.LeaveManagement.Application.Models.Identity;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
 
 namespace Hr.LeaveManagement.Api.Controllers
 {
@@ -9,5 +11,26 @@ namespace Hr.LeaveManagement.Api.Controllers
     public class AccountController : ControllerBase
     {
         private readonly IAuthService _authenticationService;
+
+        public AccountController(IAuthService authenticationService)
+        {
+            _authenticationService = authenticationService;
+        }
+      
+        [HttpPost("login")]
+        public async Task<ActionResult<AuthResponse>> Login(AuthRequest authRequest)
+        {
+            return Ok(await _authenticationService.Login(authRequest));
+        }
+
+        [HttpPost("register")]
+        public async Task<ActionResult<RegistrationResponse>> Register(RegistrationRequest registrationRequest)
+        {
+            return Ok(await _authenticationService.Register(registrationRequest));
+        }
+
+
     }
+
+    
 }
